2c94100bc86153ec6a3fb25080d3cb3216309a1e
Raphi <badapple7913@gmail.com>
2022-08-04 09:05:37 +0800

    css load OK

--- user_script.js
+++ user_script.js
@@ -7,16 +7,22 @@
 // @description Replace fonts by websites in phone kiwi browser
 // @description:zh-cn 手機kiwi瀏覽器雲端字體替換
 // @include     *
+// @resource fontcss https://cdn.jsdelivr.net/gh/Vignetter/My_web_font@main/font.css
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
 // @license     MIT
 // @run-at      document-start
 // ==/UserScript==
 
-var my_font = new FontFace('TsukuARdGothic', "url('https://cdn.jsdelivr.net/gh/Vignetter/My_web_font@main/font.css') format('truetype')", { style: 'normal', weight: 'normal' });
+const css = GM_getResourceText("fontcss");
+GM_addStyle(css);
+
+var my_font = new FontFace('TsukuARdGothic', "url('https://cdn.jsdelivr.net/gh/Vignetter/My_web_font@main/FOT-TsukuARdGothic%20Std%20D.ttf') format('truetype')", { style: 'normal', weight: 'normal' });
 
 my_font.load().then(function(loaded_face) {
 	document.fonts.add(loaded_face);
-  console.log('font loaded!')
-    // document.body.style.fontFamily = '"TsukuARdGothic", "微軟雅黑"';
+    console.log('font loaded!')
+//     document.body.style.fontFamily = '"TsukuARdGothic", "微軟雅黑"';
 }).catch(function(error) {
 	// error occurred
     console.log('Something wrong!')
